4adba5a364df6d1eb98b2212a6cd00d140cc24f7
Anth0nyPereira <anthonypereira@ua.pt>
2020-12-23 12:55:09 +0000

    Revert docker

---
+++ ProjStart/rooms_scanner/target/classes/static/js/popups.js
@@ -0,0 +1,19 @@
+
+function displaySignUp() {
+    var modal = document.getElementById('sec-e252');
+    modal.style.display = "none";
+    var modal = document.getElementById('sec-f378');
+    modal.style.display = "block";
+}
+function displayLogin() {
+    var modal = document.getElementById('sec-f378');
+    modal.style.display = "none";
+    var modal = document.getElementById('sec-e252');
+    modal.style.display = "block";
+}
+function closePopUp() {
+    var modal = document.getElementById('sec-f378');
+    modal.style.display = "none";
+    var modal = document.getElementById('sec-e252');
+    modal.style.display = "none";
+}
