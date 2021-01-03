4d71fcc83681d25212384d5c6416621f849b649a
Eleandro Laureano <eleandrog@ua.pt>
2021-01-03 19:30:04 +0000

    Login Controller

---
+++ ProjSpringboot/rooms_scanner/src/main/java/ies/p1/rooms_scanner/Controller/LoginController.java
@@ -0,0 +1,19 @@
+package ies.p1.rooms_scanner.Controller;
+
+import ies.p1.rooms_scanner.Entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @ResponseBody
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+}
