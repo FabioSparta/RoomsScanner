bcc40120826071ac06cfc7a798e57b477afddf9c
Eleandro Laureano <eleandrog@ua.pt>
2021-01-11 21:11:12 +0000

    UserRepository + Register HTML

--- ProjSpringboot/rooms_scanner/src/main/java/ies/p1/rooms_scanner/Repository/UserRepository.java
+++ ProjSpringboot/rooms_scanner/src/main/java/ies/p1/rooms_scanner/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package ies.p1.rooms_scanner.Repository;
 
 import ies.p1.rooms_scanner.Entities.User;
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User,Integer> {
+    public Optional<User> findUserByEmail(String email);
 }
